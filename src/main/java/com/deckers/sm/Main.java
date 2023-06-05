084c6e155724823a8d21a5e366a3b70d05984202
jk.wu <jk.wu@deckers.com>
2023-06-05 11:27:13 +0800

    init sm-api

--- src/main/java/com/deckers/sm/Main.java
+++ src/main/java/com/deckers/sm/Main.java
@@ -1,4 +1,6 @@
-package com.deckers.sm;public class Main {
+package com.deckers.sm;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
